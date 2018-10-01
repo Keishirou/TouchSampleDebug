1b976ff68c395f8ccc829367e262285e5b1d864f
Keishirou <Keishirou@users.noreply.github.com>
2018-10-01 14:00:41 +0900

    Revert "speedTest"
    This reverts commit c29f5886d4330760dc793e6ef168ea2663f35b56.

--- sample_motor/sample_motor.ino
+++ sample_motor/sample_motor.ino
@@ -6,11 +6,10 @@ Serial.begin(9600);
 pinMode(12, OUTPUT); // 回転方向 (HIGH/LOW)
 pinMode(9, OUTPUT); // ブレーキ (HIGH/LOW)
 pinMode(3, OUTPUT); // PWMによるスピード制御 (0-255)
-
 }
 
 void loop() {
-  int temp = 0;
+
   int vr = analogRead(A2); //A2にボリューム部の2ピンを接続。1=5V,0=GND
   
   // モーターA: 正転
@@ -18,12 +17,10 @@ void loop() {
   digitalWrite(9, LOW);
   while(vr > 0){ //一番端っこ(抵抗値がMAX)に行くまで正転
     vr = analogRead(A2);
-    analogWrite(3, SPEED - temp);
-    temp++;
+    analogWrite(3, SPEED);
     Serial.println(vr);
   }
   digitalWrite(9, HIGH); //端っこに行ったらブレーキ
-  temp=0;
   // 2秒間上記設定で回転
   delay(2000);
   
@@ -33,12 +30,11 @@ void loop() {
   digitalWrite(9, LOW);
   while(vr <1023){ //一番端っこ(抵抗値が0側。電圧が高くなるほう)に行くまで逆転
     vr = analogRead(A2);
-    analogWrite(3, SPEED-temp);
-    temp++;
+    analogWrite(3, SPEED);
     Serial.println(vr);
   }
   digitalWrite(9, HIGH); //端っこに行ったらブレーキ
   // 2秒間上記設定で回転
-  temp=0;
+  
   delay(2000);
 }
